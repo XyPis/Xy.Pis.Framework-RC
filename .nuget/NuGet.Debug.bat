9dba9374fefa1e00c63890b204eeb57292f22b19
XyPis <dragon.luo@live.cn>
2016-08-23 10:09:37 +0800

    aa

--- .nuget/NuGet.Debug.bat
+++ .nuget/NuGet.Debug.bat
@@ -1,3 +1,8 @@
+set packages=.\packages
+
+if not exist %packages% md %packages%
+@echo Successfully create '%packages%' folder
+
 NuGet pack ..\Xy.Pis.Utils\Xy.Pis.Utils.csproj -o .\packages -Build -Properties Configuration=Debug
 NuGet pack ..\Xy.Pis.Contract\Xy.Pis.Contract.csproj -o .\packages -Build -Properties Configuration=Debug
 NuGet pack ..\Xy.Pis.Core\Xy.Pis.Core.csproj -o .\packages -Build -Properties Configuration=Debug
@@ -7,3 +12,4 @@ NuGet pack ..\Xy.Pis.Proxy\Xy.Pis.Proxy.csproj -o .\packages -Build -Properties 
 
 NuGet push .\packages\*.nupkg -s http://localhost:8080 123
 Del /Q .\packages\* 
+pause
--- .nuget/NuGet.Release.bat
+++ .nuget/NuGet.Release.bat
@@ -1,3 +1,8 @@
+set packages=.\packages
+
+if not exist %packages% md %packages%
+@echo Successfully create '%packages%' folder
+
 NuGet pack ..\Xy.Pis.Utils\Xy.Pis.Utils.csproj -o .\packages -Build -Properties Configuration=Release
 NuGet pack ..\Xy.Pis.Contract\Xy.Pis.Contract.csproj -o .\packages -Build -Properties Configuration=Release
 NuGet pack ..\Xy.Pis.Core\Xy.Pis.Core.csproj -o .\packages -Build -Properties Configuration=Release
